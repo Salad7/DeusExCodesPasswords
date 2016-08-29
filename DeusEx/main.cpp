f81743eb4f4646ca6bb44e1a67fdb2590eb6ee39
Mohamed Salad <msalad@uncc.edu>
2016-08-28 21:38:40 -0400

    Added some visual details
    Error prone Listbox, with statistics

---
+++ DeusEx/main.cpp
@@ -0,0 +1,221 @@
+// GT_HelloWorldWin32.cpp
+// compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+//
+typedef struct
+{
+	TCHAR achName[MAX_PATH];
+	TCHAR achPosition[12];
+	TCHAR value[12];
+} Player;
+
+Player Roster[] =
+{
+	{ TEXT("Dubai"), TEXT("Keycode"), TEXT("0182"), }
+	
+};
+
+
+// Global variables
+
+// The main window class name.
+static TCHAR szWindowClass[] = _T("win32app");
+
+// The string that appears in the application's title bar.
+static TCHAR szTitle[] = _T("Deus Ex - Mankind Divided Passwords and Codes");
+
+HINSTANCE hInst;
+
+// Forward declarations of functions included in this code module:
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Deus Ex - Mankind Divided Passwords and Codes"),
+			NULL);
+
+		return 1;
+	}
+
+	hInst = hInstance; // Store instance handle in our global variable
+
+					   // The parameters to CreateWindow explained:
+					   // szWindowClass: the name of the application
+					   // szTitle: the text that appears in the title bar
+					   // WS_OVERLAPPEDWINDOW: the type of window to create
+					   // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+					   // 500, 100: initial size (width, length)
+					   // NULL: the parent of this window
+					   // NULL: this application does not have a menu bar
+					   // hInstance: the first parameter from WinMain
+					   // NULL: not used in this application
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Deus Ex - Mankind Divided Passwords and Codes"),
+			NULL);
+
+		return 1;
+	}
+
+	// The parameters to ShowWindow explained:
+	// hWnd: the value returned from CreateWindow
+	// nCmdShow: the fourth parameter from WinMain
+	ShowWindow(hWnd,
+		nCmdShow);
+	UpdateWindow(hWnd);
+
+	// Main message loop:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	TCHAR greeting[] = _T("Hello, World!");
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		// Here your application is laid out.
+		// For this introduction, we just print out "Hello, World!"
+		// in the top left corner.
+		TextOut(hdc,
+			5, 5,
+			greeting, _tcslen(greeting));
+		// End application-specific layout section.
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+INT_PTR CALLBACK ListBoxExampleProc(HWND hDlg, UINT message,
+	WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+	{
+		// Add items to list. 
+		HWND hwndList = GetDlgItem(hDlg, IDC_LISTBOX_EXAMPLE);
+		for (int i = 0; i < ARRAYSIZE(Roster); i++)
+		{
+			int pos = (int)SendMessage(hwndList, LB_ADDSTRING, 0,
+				(LPARAM)Roster[i].achName);
+			// Set the array index of the player as item data.
+			// This enables us to retrieve the item from the array
+			// even after the items are sorted by the list box.
+			SendMessage(hwndList, LB_SETITEMDATA, pos, (LPARAM)i);
+		}
+		// Set input focus to the list box.
+		SetFocus(hwndList);
+		return TRUE;
+	}
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		case IDCANCEL:
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+
+		case IDC_LISTBOX_EXAMPLE:
+		{
+			switch (HIWORD(wParam))
+			{
+			case LBN_SELCHANGE:
+			{
+				HWND hwndList = GetDlgItem(hDlg, IDC_LISTBOX_EXAMPLE);
+
+				// Get selected index.
+				int lbItem = (int)SendMessage(hwndList, LB_GETCURSEL, 0, 0);
+
+				// Get item data.
+				int i = (int)SendMessage(hwndList, LB_GETITEMDATA, lbItem, 0);
+
+				// Do something with the data from Roster[i]
+				TCHAR buff[MAX_PATH];
+				StringCbPrintf(buff, ARRAYSIZE(buff),
+					TEXT("Location: %s\nType played: %d\nValue: %d"),
+					Roster[i].achPosition, Roster[i].achName,
+					Roster[i].value);
+
+				SetDlgItemText(hDlg, IDC_STATISTICS, buff);
+				return TRUE;
+			}
+			}
+		}
+		return TRUE;
+		}
+	}
+	return FALSE;
+}
+
